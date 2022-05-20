bf8db48dd7a2f1956415fee2001934fdef8ff2c8
Jesper <jesper.olsson@iths.se>
2022-05-20 11:07:30 +0200

    Add 404 page for unhandled routes in client

--- client/src/components/Button/index.tsx
+++ client/src/components/Button/index.tsx
@@ -5,12 +5,13 @@ interface Props {
   children: React.ReactNode
   style?: string
   btnState?: boolean
+  errorPage?: any
 }
 
 const Button = (props:Props) => {
   return (
     <Wrapper>
-      <PushableBtn largerBtn={props.style} disabled={props.btnState}>
+      <PushableBtn onClick={() => props.errorPage ? props.errorPage(-1) : null} largerBtn={props.style} disabled={props.btnState}>
         <FrontBtn>{props.children}</FrontBtn>
       </PushableBtn>
       </Wrapper>
---
+++ client/src/components/PageNotFound/style.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const StyledWrapper = styled.div`
+  display: flex;
+  min-height: 100vh;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`
+
+export const ErrorStatus = styled.h1`
+
+`
+
+export const ErrorMessage = styled.h2`
+  margin-bottom: 3rem;
+`
